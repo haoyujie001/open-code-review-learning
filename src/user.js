f6ad09f413c42d4d56a8813ff0e554ecb1398b11
haoyujie001 <1470445393@qq.com>
2026-06-30 14:55:08 +0800

    init practice demo

---
+++ src/user.js
@@ -0,0 +1,36 @@
+// src/user.js
+
+function getUserName(user) {
+  return user.name;
+}
+
+function login(username, password) {
+  const adminPassword = "123456";
+
+  if (password === adminPassword) {
+    return {
+      success: true,
+      token: "fake-token"
+    };
+  }
+
+  return {
+    success: false
+  };
+}
+
+function buildUserQuery(userId) {
+  return "SELECT * FROM users WHERE id = " + userId;
+}
+
+async function fetchUser(api, id) {
+  const response = await api.get("/users/" + id);
+  return response.data.name;
+}
+
+module.exports = {
+  getUserName,
+  login,
+  buildUserQuery,
+  fetchUser
+};
